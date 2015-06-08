6dd70ea2fea308149505894f81ef5966d412f13c
heri@madmedia.ca <heri@madmedia.ca>
2015-06-08 17:05:58 -0400

    resolve

--- js/movieApp.js
+++ js/movieApp.js
@@ -101,7 +101,7 @@ app.directive('movies', function() {
 
 app.directive('login', function() {
 		return {
-			templateUrl: '../partials/login.html'
+			templateUrl: '../partials/login.html',
    			 controller: 'movieController'
 		};
 	});
